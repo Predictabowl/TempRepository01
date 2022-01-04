6ef25efb2ebd9233d9b887711fdc8c77b4f22a16
Piero <piero.aldinucci@gmail.com>
2022-01-04 20:26:39 +0100

    first commit

---
+++ src/main/java/com/example/demo/SpringbootLearning03Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearning03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearning03Application.class, args);
+	}
+
+}
