1021ed0ff0f610c95ca2d7202d5e2f2f48151e50
Piero <piero.aldinucci@gmail.com>
2022-01-07 19:29:21 +0100

    Added 1 model class

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,64 @@
+package com.example.demo.model;
+
+import java.util.Objects;
+
+public class Employee {
+	
+	private String id;
+	private String name;
+	private long salary;
+	
+	public Employee(String id, String name, long salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, salary);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name) && salary == other.salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+}
