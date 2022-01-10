733f8310ff0b2a7c3d252e132832987c9977257d
Piero <piero.aldinucci@gmail.com>
2022-01-10 14:54:17 +0100

    Custom Repository queries

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -2,11 +2,22 @@ package com.example.demo.model;
 
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 	
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String name;
 	private long salary;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
 	
 	public Employee(Long id, String name, long salary) {
 		super();
